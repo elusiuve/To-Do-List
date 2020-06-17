cc7909cbaf4771151cc0a0e4004bebe5cfc57710
elusiuve <elusiuve15.09.93@gmail.com>
2020-06-17 16:58:24 +0300

    Updates

---
+++ precache-manifest.c2dfa0b2ca5155307f80251a4d57dc76.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "760cbb1fefa42055bb09430218d0eb47",
+    "url": "/To-Do-List/index.html"
+  },
+  {
+    "revision": "c3253d6c1a8ec69187df",
+    "url": "/To-Do-List/static/css/main.8601e187.chunk.css"
+  },
+  {
+    "revision": "d506f1b20942b2f6ac49",
+    "url": "/To-Do-List/static/js/2.7c73f1da.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/To-Do-List/static/js/2.7c73f1da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3253d6c1a8ec69187df",
+    "url": "/To-Do-List/static/js/main.8ea00d1d.chunk.js"
+  },
+  {
+    "revision": "52f9d049c38798a7a51b",
+    "url": "/To-Do-List/static/js/runtime-main.c55e750b.js"
+  }
+]);
